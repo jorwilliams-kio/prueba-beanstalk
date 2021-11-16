b1bb6e4f70ce8a090751fc2687bf19e3908e2c52
Jorge Williams <jwilliams@kionetworks.com>
2021-11-16 16:03:36 -0600

    Adapted for RDS

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
 app.get('/', (req, res) => {
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+  });
+  connection.end();
+
   res.send("Prueba Beanstalk exitosa");
 });
 
