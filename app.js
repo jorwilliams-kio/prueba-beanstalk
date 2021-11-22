5f7bfb640af406bcf70fa664650946c9b918a6f7
Jorge Williams <jwilliams@kionetworks.com>
2021-11-22 14:07:07 -0600

    HC EB

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 let mysql = require('mysql');
 
+// const connection = mysql.createConnection({
+//   host     : process.env.RDS_HOSTNAME,
+//   user     : process.env.RDS_USERNAME,
+//   password : process.env.RDS_PASSWORD,
+//   port     : process.env.RDS_PORT,
+// });
+
 const connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT,
+  host     : "ebdb.csitdsxpoq37.us-east-2.rds.amazonaws.com",
+  user     : "admin",
+  password : "EBDBjw.93",
+  port     : "3306",
 });
 
 const app = express();
