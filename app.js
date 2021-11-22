625c94b5c6b5a7a838d30d432b53a7a8f91541ee
Jorge Williams <jwilliams@kionetworks.com>
2021-11-21 20:07:05 -0600

    Simple fix

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var connection = mysql.createConnection({
   host     : process.env.RDS_HOSTNAME,
   user     : process.env.RDS_USERNAME,
   password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
+  port     : process.env.RDS_PORT,
+  database : process.env.RDS_DB_NAME
 });
 
 const app = express();
@@ -14,12 +15,15 @@ app.get('/', (req, res) => {
   connection.connect(function(err) {
     if (err) {
       console.error('Database connection failed: ' + err.stack);
+      console.error('Database message: ' + err.message);
       return;
     }
-  
+
     console.log('Connected to database !!!!.');
   });
   
+  // connection.query('SELECT * FROM CATEGORIES')
+
   connection.end();
   res.send("Prueba Beanstalk exitosa");
 });
