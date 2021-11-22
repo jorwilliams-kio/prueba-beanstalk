7d2ee5da2c14853da6c010d6c83d9cfb4e2de10b
Jorge Williams <jwilliams@kionetworks.com>
2021-11-22 17:08:54 -0600

    Hidden creds2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ connection.connect(function(err) {
   console.log('Connected to database !!!!.');
 });
 
-const data = connection.query('SELECT * FROM CONTEST_TYPES;');
+const data = connection.query('USE EBDB; SELECT * FROM CONTEST_TYPES;');
 connection.end();
 
 
