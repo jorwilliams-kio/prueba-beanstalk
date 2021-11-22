4b2e0bbf9c2684fe9438c5001d782d3ce20d8e16
Jorge Williams <jwilliams@kionetworks.com>
2021-11-22 17:03:14 -0600

    Hidden creds

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
 const express = require('express');
-const { json } = require('express/lib/response');
+// const { json } = require('express/lib/response');
 let mysql = require('mysql');
 
-// const connection = mysql.createConnection({
-//   host     : process.env.RDS_HOSTNAME,
-//   user     : process.env.RDS_USERNAME,
-//   password : process.env.RDS_PASSWORD,
-//   port     : process.env.RDS_PORT,
-// });
-
 const connection = mysql.createConnection({
-  host     : "ebdb.csitdsxpoq37.us-east-2.rds.amazonaws.com",
-  user     : "admin",
-  password : "EBDBjw.93",
-  port     : "3306",
-  database : "EBDB"
+  host     : process.env.testhost,
+  user     : process.env.testusername,
+  password : process.env.testpassword,
+  port     : process.env.testport,
 });
+
+// const connection = mysql.createConnection({
+//   host     : "ebdb.csitdsxpoq37.us-east-2.rds.amazonaws.com",
+//   user     : "admin",
+//   password : "EBDBjw.93",
+//   port     : "3306",
+//   database : "EBDB"
+// });
 
 const app = express();
 // app.use(express.json())
 
+connection.connect(function(err) {
+  if (err) {
+    // console.error('Database connection failed: ' + err.stack);
+    console.error('Database message: ' + err.message);
+    return;
+  }
+  
+  console.log('Connected to database !!!!.');
+});
+
+const data = connection.query('SELECT * FROM CONTEST_TYPES;');
+connection.end();
 
 
 app.get('/', (req, res) => {
-  connection.connect(function(err) {
-    if (err) {
-      // console.error('Database connection failed: ' + err.stack);
-      console.error('Database message: ' + err.message);
-      return;
-    }
-    
-    console.log('Connected to database !!!!.');
-  });
-  
-  // const data = connection.query('SELECT * FROM CONTEST_TYPES;')
-  connection.end();
-  
+  console.log(data);
   // res.json(data);
-  res.send("Prueba exitosa")
+  res.send("Prueba exitosa");
 });
 
 const port = process.env.port || 3000
 app.listen(port, () => {
   console.log("Server up");
-})
+});
